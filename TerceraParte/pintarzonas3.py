282d37cb5ba5138c9b84ad37496bb528af5bdb29
Jorfuje <arbjorfuje@gmail.com>
2024-02-29 22:12:58 -0600

    HS en 8

--- TerceraParte/pintarzonas3.py
+++ TerceraParte/pintarzonas3.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 window = "Lucas-Kanade Optical Flow"
-video_path = "TerceraParte/caida.mp4"
+video_path = "TerceraParte/mujer.mp4"
 output_video_path = "videos/LK.mp4"
 
 capture = cv2.VideoCapture(video_path)
